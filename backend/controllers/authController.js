888ac85d53715e994a3818607477cc1e42715b25
Tanmay Sahu <sahutanmay1903@gmail.com>
2026-02-15 22:34:35 +0530

    fixed transporter

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,31 +3,33 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import validator from "validator";
 import nodemailer from "nodemailer";
-import { google } from "googleapis";
+// import { google } from "googleapis";
 
-const OAuth2 = google.auth.OAuth2;
+// const OAuth2 = google.auth.OAuth2;
 
-const oauth2Client = new OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  "https://developers.google.com/oauthplayground"
-);
+// const oauth2Client = new OAuth2(
+//   process.env.CLIENT_ID,
+//   process.env.CLIENT_SECRET,
+//   "https://developers.google.com/oauthplayground"
+// );
 
-oauth2Client.setCredentials({
-  refresh_token: process.env.REFRESH_TOKEN,
-});
+// oauth2Client.setCredentials({
+//   refresh_token: process.env.REFRESH_TOKEN,
+// });
 
-const accessToken = await oauth2Client.getAccessToken();
+// const accessToken = await oauth2Client.getAccessToken();
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    type: "OAUTH2",
+    // type: "OAUTH2",
+    // user: process.env.EMAIL_USER,
+    // clientId: process.env.CLIENT_ID,
+    // clientSecret: process.env.CLIENT_SECRET,
+    // refreshToken: process.env.REFRESH_TOKEN,
+    // accessToken: accessToken.token,
     user: process.env.EMAIL_USER,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
-    accessToken: accessToken.token,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
