487eb0d2c52aa2d094a2aad95d805b0d88c4ecef
Tanmay Sahu <sahutanmay1903@gmail.com>
2026-02-15 22:15:01 +0530

    fixed transporter

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,21 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import validator from "validator";
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
+
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN,
+});
+
+const accessToken = await oauth2Client.getAccessToken();
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -12,6 +27,7 @@ const transporter = nodemailer.createTransport({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     refreshToken: process.env.REFRESH_TOKEN,
+    accessToken: accessToken.token,
   },
 });
 
