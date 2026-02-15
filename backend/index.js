9efb36a206952da18fbe4ca04554b5217ccccb6c
Tanmay Sahu <sahutanmay1903@gmail.com>
2026-02-15 20:58:25 +0530

    cors fixed

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import e from "express";
 import "dotenv/config";
 import connectDB from "./config/db.js";
-import cors from "cors"
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 
 const app = e();
@@ -9,17 +9,20 @@ const port = process.env.PORT;
 
 connectDB();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://authentication-system-orpin.vercel.app"],
+    credentials: true,
+  }),
+);
+
 app.use(e.json());
 
 app.get("/", (req, res) => {
   res.send("Everything's Good");
 });
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log("Backend Started on", port);
