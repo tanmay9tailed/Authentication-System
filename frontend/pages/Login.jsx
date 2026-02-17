970dd62e4dae1a940661811ca6357bb1aa3e5935
Tanmay Sahu <sahutanmay1903@gmail.com>
2026-02-17 19:21:10 +0530

    added vercel.json

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -58,7 +58,7 @@ export default function Login() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-blue-50 to-gray-100">
       <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md transition-all duration-300 hover:shadow-xl">
         <h2 className="text-3xl font-bold text-center mb-6">Login</h2>
 
